850136e831eb0f19b07ba96463b449a8e5aaa13c
Mark Notton <mark@yellostudio.co.uk>
2016-07-08 16:00:06 +0100

    First Commit

---
+++ custom-props-extension.js
@@ -0,0 +1,46 @@
+(function( $ ) {
+
+  $.customproperty = function(property) {
+    return $('body').customproperty(property);
+  }
+
+  $.fn.extend({
+    customproperty : function(property) {
+
+
+      // Perform a few checks to make sure the selector that is being checked is valid
+      var selector = this.selector !== undefined && this.selector !== null && $(this.selector).length ? this.selector : null;
+
+      if (selector) {
+
+        if (property) {
+          if ($.isArray(property)) {
+
+            var properties = [];
+
+            $.each(property, function( index, value ) {
+
+              value = CustomProps.read($(selector), value);
+
+              if ($.isNumeric(value)) {
+                value = parseInt(value, 10);
+              };
+
+              properties.push(value);
+            });
+
+            return properties;
+
+          } else {
+
+            return CustomProps.read($(selector), property);
+
+          }
+        } else {
+          // Return all custom properties for this selector
+          return CustomProps.data($(this.selector));
+        }
+      }
+    }
+  });
+}( jQuery ));
